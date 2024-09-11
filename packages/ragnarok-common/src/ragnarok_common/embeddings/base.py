e35d8e24913748f8a35fceb9fb2cd82e275fdab0
Patryk Wyżgowski <patryk.wyzgowski@deepsense.ai>
2024-09-11 10:01:25 +0200

    feat: add embeddings

---
+++ packages/ragnarok-common/src/ragnarok_common/embeddings/base.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+class Embeddings(ABC):
+    """
+    Abstract client for communication with embedding models.
+    """
+
+    @abstractmethod
+    async def embed_text(self, data: list[str]) -> list[list[float]]:
+        """
+        Creates embeddings for the given strings.
+
+        Args:
+            data: List of strings to get embeddings for.
+
+        Returns:
+            List of embeddings for the given strings.
+        """
