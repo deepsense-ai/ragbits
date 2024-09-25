83c02723ea7b4b285d99389b9a5596aefedf699c
Ludwik Trammer <ludwik.trammer@deepsense.ai>
2024-09-25 13:48:02 +0200

    feat(prompt-discovery): Look for prompts in arbitrary files using patterns

--- packages/ragbits-dev-kit/tests/unit/discovery/test_prompt_discovery.py
+++ packages/ragbits-dev-kit/tests/unit/discovery/test_prompt_discovery.py
@@ -1,38 +1,30 @@
-import sys
 from pathlib import Path
 
 from ragbits.dev_kit.prompt_lab.discovery.prompt_discovery import PromptDiscovery
 
+current_dir = str(Path(__file__).parent)
+
 
 def test_prompt_discovery_from_file():
-    test_paths = ["prompt_classes_for_tests.py"]
+    discovery_results = PromptDiscovery(root_path=current_dir).discover()
+    print(discovery_results)
 
-    discovery_result = PromptDiscovery(test_paths).discover()
+    assert len(discovery_results) == 5
 
-    assert len(discovery_result.keys()) == 4
-
-    assert "PromptForTest" in discovery_result.keys()
-    assert discovery_result["PromptForTest"]["user_prompt"] == "fake user prompt"
-    assert discovery_result["PromptForTest"]["system_prompt"] == "fake system prompt"
-    assert len(discovery_result["PromptForTest"]["input_type"].model_fields) == 6
-
-    assert "PromptForTest2" in discovery_result.keys()
-    assert discovery_result["PromptForTest2"]["user_prompt"] == "fake user prompt2"
-    assert discovery_result["PromptForTest2"]["system_prompt"] == "fake system prompt2"
-    assert len(discovery_result["PromptForTest2"]["input_type"].model_fields) == 1
-
-    assert "MyPromptWithBase" in discovery_result.keys()
-    assert discovery_result["MyPromptWithBase"]["user_prompt"] == "custom user prompt"
-    assert discovery_result["MyPromptWithBase"]["system_prompt"] == "my base system prompt"
+    class_names = [cls.__name__ for cls in discovery_results]
+    assert "PromptForTest" in class_names
+    assert "PromptForTest2" in class_names
+    assert "PromptWithoutInput" in class_names
+    assert "PromptForTestInput" not in class_names
 
 
 def test_prompt_discovery_from_package():
-    sys.path.append(str(Path(__file__).parent))
-    test_paths = ["ragbits_tests_pkg_with_prompts"]
+    discovery_results = PromptDiscovery(
+        root_path=current_dir, file_pattern="ragbits_tests_pkg_with_prompts/**/*.py"
+    ).discover()
 
-    discovery_result = PromptDiscovery(test_paths).discover()
+    assert len(discovery_results) == 2
 
-    assert len(discovery_result.keys()) == 2
-
-    assert "PromptForTestA" in discovery_result
-    assert "PromptForTestB" in discovery_result
+    class_names = [cls.__name__ for cls in discovery_results]
+    assert "PromptForTestA" in class_names
+    assert "PromptForTestB" in class_names
