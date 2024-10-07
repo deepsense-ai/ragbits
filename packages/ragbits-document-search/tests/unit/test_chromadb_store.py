704eef2445895092f3f1ef05a0b00c24c0325780
PatrykWyzgowski <pwyzgow.github@gmail.com>
2024-10-07 09:04:27 +0200

    fix(document-search): avoid metadata mutation (#63)

--- packages/ragbits-document-search/tests/unit/test_chromadb_store.py
+++ packages/ragbits-document-search/tests/unit/test_chromadb_store.py
@@ -71,15 +71,6 @@ def test_get_chroma_collection(mock_chromadb_store):
     assert mock_chromadb_store._chroma_client.get_or_create_collection.called
 
 
-def test_get_chroma_collection_with_custom_embedding_function(
-    custom_embedding_function, mock_chromadb_store_with_custom_embedding_function, mock_chroma_client
-):
-    mock_chroma_client.get_or_create_collection.assert_called_once_with(
-        name="test_index",
-        metadata={"hnsw:space": "l2"},
-    )
-
-
 async def test_stores_entries_correctly(mock_chromadb_store):
     data = [
         VectorDBEntry(
@@ -96,17 +87,15 @@ async def test_stores_entries_correctly(mock_chromadb_store):
 
 
 def test_process_db_entry(mock_chromadb_store, mock_vector_db_entry):
-    id, embedding, text, metadata = mock_chromadb_store._process_db_entry(mock_vector_db_entry)
-    print(f"metadata: {metadata}, type: {type(metadata)}")
+    id, embedding, metadata = mock_chromadb_store._process_db_entry(mock_vector_db_entry)
 
     assert id == sha256(b"test_key").hexdigest()
     assert embedding == [0.1, 0.2, 0.3]
-    assert text == "test content"
     assert (
-        metadata["document"]
-        == '{"title": "test title", "source": {"path": "/test/path"}, "document_type": "test_type"}'
+        metadata["__metadata"]
+        == '{"content": "test content", "document": {"title": "test title", "source": {"path": "/test/path"}, "document_type": "test_type"}}'
     )
-    assert metadata["key"] == "test_key"
+    assert metadata["__key"] == "test_key"
 
 
 async def test_store(mock_chromadb_store, mock_vector_db_entry):
@@ -122,9 +111,8 @@ async def test_retrieves_entries_correctly(mock_chromadb_store):
         "metadatas": [
             [
                 {
-                    "key": "test_key",
-                    "content": "test content",
-                    "document": {"title": "test title", "source": {"path": "/test/path"}, "document_type": "test_type"},
+                    "__key": "test_key",
+                    "__metadata": '{"content": "test content", "document": {"title": "test title", "source": {"path": "/test/path"}, "document_type": "test_type"}}',
                 }
             ]
         ],
@@ -143,27 +131,6 @@ async def test_handles_empty_retrieve(mock_chromadb_store):
     assert len(entries) == 0
 
 
-async def test_find_similar(mock_chromadb_store, mock_embedding_function):
-    mock_embedding_function.embed_text.return_value = [[0.1, 0.2, 0.3]]
-    mock_chromadb_store._embedding_function = mock_embedding_function
-    mock_chromadb_store._chroma_client.get_or_create_collection().query.return_value = {
-        "documents": [["test content"]],
-        "distances": [[0.1]],
-    }
-    result = await mock_chromadb_store.find_similar("test text")
-    assert result == "test content"
-
-
-async def test_find_similar_with_custom_embeddings(mock_chromadb_store, custom_embedding_function):
-    mock_chromadb_store._embedding_function = custom_embedding_function
-    mock_chromadb_store._chroma_client.get_or_create_collection().query.return_value = {
-        "documents": [["test content"]],
-        "distances": [[0.1]],
-    }
-    result = await mock_chromadb_store.find_similar("test text")
-    assert result == "test content"
-
-
 def test_repr(mock_chromadb_store):
     assert repr(mock_chromadb_store) == "ChromaDBStore(index_name=test_index)"
 
@@ -180,14 +147,3 @@ def test_return_best_match(mock_chromadb_store, retrieved, max_distance, expecte
     mock_chromadb_store._max_distance = max_distance
     result = mock_chromadb_store._return_best_match(retrieved)
     assert result == expected
-
-
-def test_is_json_valid_string(mock_chromadb_store):
-    # Arrange
-    valid_json_string = '{"key": "value"}'
-
-    # Act
-    result = mock_chromadb_store._is_json(valid_json_string)
-
-    # Assert
-    assert result is True
