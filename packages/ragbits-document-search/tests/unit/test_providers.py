5398d20676d15652d18b394321a440a3648d3524
Alan Konarski <129968242+akonarski-ds@users.noreply.github.com>
2024-10-08 09:44:12 +0200

    feat(document-search): async unstructured api (#37)

--- packages/ragbits-document-search/tests/unit/test_providers.py
+++ packages/ragbits-document-search/tests/unit/test_providers.py
@@ -1,12 +1,11 @@
+import os
+from unittest.mock import patch
+
 import pytest
 
 from ragbits.document_search.documents.document import DocumentMeta, DocumentType
 from ragbits.document_search.ingestion.providers.base import DocumentTypeNotSupportedError
-from ragbits.document_search.ingestion.providers.unstructured import (
-    UNSTRUCTURED_API_KEY_ENV,
-    UNSTRUCTURED_API_URL_ENV,
-    UnstructuredProvider,
-)
+from ragbits.document_search.ingestion.providers.unstructured import UnstructuredProvider
 
 
 @pytest.mark.parametrize("document_type", UnstructuredProvider.SUPPORTED_DOCUMENT_TYPES)
@@ -21,20 +20,21 @@ def test_unsupported_provider_validates_supported_document_types_fails():
     assert "Document type unknown is not supported by the UnstructuredProvider" in str(err.value)
 
 
+@patch.dict(os.environ, {}, clear=True)
 async def test_unstructured_provider_raises_value_error_when_api_key_not_set():
     with pytest.raises(ValueError) as err:
         await UnstructuredProvider().process(
             DocumentMeta.create_text_document_from_literal("Name of Peppa's brother is George.")
         )
 
-    assert f"{UNSTRUCTURED_API_KEY_ENV} environment variable is not set" in str(err.value)
+    assert "Either pass api_key argument or set the UNSTRUCTURED_API_KEY environment variable" == str(err.value)
 
 
-async def test_unstructured_provider_raises_value_error_when_api_url_not_set(monkeypatch: pytest.MonkeyPatch):
-    monkeypatch.setenv(UNSTRUCTURED_API_KEY_ENV, "dummy_key")
+@patch.dict(os.environ, {}, clear=True)
+async def test_unstructured_provider_raises_value_error_when_server_url_not_set():
     with pytest.raises(ValueError) as err:
-        await UnstructuredProvider().process(
+        await UnstructuredProvider(api_key="api_key").process(
             DocumentMeta.create_text_document_from_literal("Name of Peppa's brother is George.")
         )
 
-    assert f"{UNSTRUCTURED_API_URL_ENV} environment variable is not set" in str(err.value)
+    assert "Either pass api_server argument or set the UNSTRUCTURED_SERVER_URL environment variable" == str(err.value)
