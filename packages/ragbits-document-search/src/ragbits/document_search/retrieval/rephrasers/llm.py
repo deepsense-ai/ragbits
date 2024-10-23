17f9ac24a0faf878c40ba420636e9a46c01c00a7
akotyla <79326805+akotyla@users.noreply.github.com>
2024-10-23 10:58:10 +0200

    feat(document-search): LLM-based query rephraser (#115)

---
+++ packages/ragbits-document-search/src/ragbits/document_search/retrieval/rephrasers/llm.py
@@ -0,0 +1,67 @@
+from typing import Any
+
+from ragbits.core.llms import get_llm
+from ragbits.core.llms.base import LLM
+from ragbits.core.prompt import Prompt
+from ragbits.document_search.retrieval.rephrasers.base import QueryRephraser
+from ragbits.document_search.retrieval.rephrasers.prompts import (
+    QueryRephraserInput,
+    QueryRephraserPrompt,
+    get_rephraser_prompt,
+)
+
+
+class LLMQueryRephraser(QueryRephraser):
+    """
+    A rephraser class that uses a LLM to rephrase queries.
+    """
+
+    def __init__(self, llm: LLM, prompt: type[Prompt[QueryRephraserInput, Any]] | None = None):
+        """
+        Initialize the LLMQueryRephraser with a LLM.
+
+        Args:
+            llm: A LLM instance to handle query rephrasing.
+            prompt: The prompt to use for rephrasing queries.
+        """
+        self._llm = llm
+        self._prompt = prompt or QueryRephraserPrompt
+
+    async def rephrase(self, query: str) -> list[str]:
+        """
+        Rephrase a given query using the LLM.
+
+        Args:
+            query: The query to be rephrased. If not provided, a custom prompt must be given.
+
+        Returns:
+            A list containing the rephrased query.
+
+        Raises:
+            LLMConnectionError: If there is a connection error with the LLM API.
+            LLMStatusError: If the LLM API returns an error status code.
+            LLMResponseError: If the LLM API response is invalid.
+        """
+        input_data = self._prompt.input_type(query=query)  # type: ignore
+        prompt = self._prompt(input_data)
+        response = await self._llm.generate(prompt)
+        return response if isinstance(response, list) else [response]
+
+    @classmethod
+    def from_config(cls, config: dict) -> "LLMQueryRephraser":
+        """
+        Create an instance of `LLMQueryRephraser` from a configuration dictionary.
+
+        Args:
+            config: A dictionary containing configuration settings for the rephraser.
+
+        Returns:
+            An instance of the rephraser class initialized with the provided configuration.
+
+        Raises:
+           KeyError: If the configuration dictionary does not contain the required keys.
+           ValueError: If the prompt class is not a subclass of `Prompt` or the LLM class is not a subclass of `LLM`.
+        """
+        llm = get_llm(config["llm"])
+        prompt_cls = get_rephraser_prompt(prompt) if (prompt := config.get("prompt")) else None
+        return cls(llm=llm, prompt=prompt_cls)
---
+++ packages/ragbits-document-search/src/ragbits/document_search/retrieval/rephrasers/prompts.py
@@ -0,0 +1,54 @@
+import sys
+from typing import Any
+
+from pydantic import BaseModel
+
+from ragbits.core.prompt.prompt import Prompt
+from ragbits.core.utils.config_handling import get_cls_from_config
+
+module = sys.modules[__name__]
+
+
+class QueryRephraserInput(BaseModel):
+    """
+    Input data for the query rephraser prompt.
+    """
+
+    query: str
+
+
+class QueryRephraserPrompt(Prompt[QueryRephraserInput, str]):
+    """
+    A prompt class for generating a rephrased version of a user's query using a LLM.
+    """
+
+    user_prompt = "{{ query }}"
+    system_prompt = (
+        "You are an expert in query rephrasing and clarity improvement. "
+        "Your task is to return a single paraphrased version of a user's query, "
+        "correcting any typos, handling abbreviations and improving clarity. "
+        "Focus on making the query more precise and readable while keeping its original intent.\n\n"
+        "Just return the rephrased query. No additional explanations are needed."
+    )
+
+
+def get_rephraser_prompt(prompt: str) -> type[Prompt[QueryRephraserInput, Any]]:
+    """
+    Initializes and returns a QueryRephraser object based on the provided configuration.
+
+    Args:
+        prompt: The prompt class to use for rephrasing queries.
+
+    Returns:
+        An instance of the specified QueryRephraser class, initialized with the provided config
+        (if any) or default arguments.
+
+    Raises:
+        ValueError: If the prompt class is not a subclass of `Prompt`.
+    """
+    prompt_cls = get_cls_from_config(prompt, module)
+
+    if not issubclass(prompt_cls, Prompt):
+        raise ValueError(f"Invalid rephraser prompt class: {prompt_cls}")
+
+    return prompt_cls
