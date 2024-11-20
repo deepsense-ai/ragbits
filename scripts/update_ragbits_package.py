c26722f7829a06a7f645d09cf45c12c650b8252d
Maciej Kurzawa <68014263+mackurzawa@users.noreply.github.com>
2024-11-20 15:43:33 +0100

    chore: install extras from ragbits index package (#199)

--- scripts/update_ragbits_package.py
+++ scripts/update_ragbits_package.py
@@ -181,6 +181,33 @@ def _create_changelog_release(pkg_name: str, new_version: str) -> None:
     changelog_path.write_text(changelog_content)
 
 
+def _update_ragbits_extras(packages: list[str]) -> None:
+    subpackages = [pkg for pkg in packages if pkg != "ragbits"]
+
+    extras = {}
+    for pkg in subpackages:
+        pkg_pyproject_path = PACKAGES_DIR / pkg / "pyproject.toml"
+        pkg_pyproject = tomlkit.parse(pkg_pyproject_path.read_text())
+
+        if "optional-dependencies" in pkg_pyproject["project"]:
+            for extra, deps in pkg_pyproject["project"]["optional-dependencies"].items():
+                if extra in extras and extras[extra] != deps:
+                    raise Exception(
+                        f"Duplicate extras: '{extra}' exists in multiple packages with different dependencies."
+                    )
+                extras[extra] = deps
+
+    extras = dict(sorted(extras.items()))
+
+    ragbits_pyproject_path = PACKAGES_DIR / "ragbits" / "pyproject.toml"
+    ragbits_pyproject_data = tomlkit.parse(ragbits_pyproject_path.read_text())
+
+    for extra, deps in extras.items():
+        ragbits_pyproject_data["project"]["optional-dependencies"][extra] = deps
+
+    ragbits_pyproject_path.write_text(tomlkit.dumps(ragbits_pyproject_data))
+
+
 def run(pkg_name: str | None = typer.Argument(None), update_type: str | None = typer.Argument(None)) -> None:
     """
     Main entry point for the package version updater. Updates package versions based on user input.
@@ -214,6 +241,7 @@ def run(pkg_name: str | None = typer.Argument(None), update_type: str | None = t
 
     if pkg_name == "ragbits":
         _update_pkg_version(pkg_name, update_type=casted_update_type)
+        _update_ragbits_extras(packages)
 
     elif pkg_name == "ragbits-core":
         if user_prompt_required:
@@ -223,6 +251,7 @@ def run(pkg_name: str | None = typer.Argument(None), update_type: str | None = t
             is_continue = True
 
         if is_continue:
+            _update_ragbits_extras(packages)
             version, new_version = _update_pkg_version(pkg_name, update_type=casted_update_type)
             casted_update_type = _check_update_type(version, new_version)
 
