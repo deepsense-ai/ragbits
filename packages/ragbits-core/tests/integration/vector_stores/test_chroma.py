cb2d051e34a7cde1f7dc81cf6e85ce9e18db69ce
mackurzawa <mac.kurzawa@gmail.com>
2024-11-28 15:41:39 +0100

    refactor: optimize integration tests for better readability and efficiency

--- packages/ragbits-core/tests/integration/vector_stores/test_chroma.py
+++ packages/ragbits-core/tests/integration/vector_stores/test_chroma.py
@@ -1,6 +1,3 @@
-import os
-import tempfile
-from pathlib import Path
 from unittest.mock import AsyncMock
 
 from chromadb import EphemeralClient
@@ -8,6 +5,7 @@ from chromadb import EphemeralClient
 from ragbits.core.vector_stores.chroma import ChromaVectorStore
 from ragbits.document_search import DocumentSearch
 from ragbits.document_search.documents.document import DocumentMeta
+from ragbits.document_search.documents.sources import LocalFileSource
 
 
 async def test_update_document() -> None:
@@ -15,16 +13,8 @@ async def test_update_document() -> None:
     document_2_content = "This is another test sentence and it should be removed from the vector store"
     document_2_new_content = "This is one more test sentence and it should be added to the vector store"
 
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".txt", mode="w", encoding="utf-8") as file_1:
-        file_1.write(document_1_content)
-        document_1_path = file_1.name
-
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".txt", mode="w", encoding="utf-8") as file_2:
-        file_2.write(document_2_content)
-        document_2_path = file_2.name
-
-    document_1 = DocumentMeta.from_local_path(Path(document_1_path))
-    document_2 = DocumentMeta.from_local_path(Path(document_2_path))
+    document_1 = DocumentMeta.create_text_document_from_literal(document_1_content)
+    document_2 = DocumentMeta.create_text_document_from_literal(document_2_content)
 
     embedder = AsyncMock()
     embedder.embed_text.return_value = [[0.0], [0.0]]
@@ -38,24 +28,15 @@ async def test_update_document() -> None:
     )
     await document_search.ingest([document_1, document_2])
 
+    if isinstance(document_2.source, LocalFileSource):
+        document_2_path = document_2.source.path
     with open(document_2_path, "w") as file:
         file.write(document_2_new_content)
 
     await document_search.ingest([document_2])
 
-    os.remove(document_1_path)
-    os.remove(document_2_path)
+    document_contents = {entry.key for entry in await vector_store.list()}
 
-    document_1_present = False
-    document_2_old_present = False
-    document_2_new_present = False
-    for entry in await vector_store.list():
-        if entry.key == document_1_content:
-            document_1_present = True
-        elif entry.key == document_2_content:
-            document_2_old_present = True
-        elif entry.key == document_2_new_content:
-            document_2_new_present = True
-    assert document_1_present
-    assert document_2_new_present
-    assert not document_2_old_present
+    assert document_1_content in document_contents
+    assert document_2_new_content in document_contents
+    assert document_2_content not in document_contents
