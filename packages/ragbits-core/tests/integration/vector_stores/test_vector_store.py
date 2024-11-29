e84f1105be74f7ac93b8b8aa43731ea0314aee57
mackurzawa <mac.kurzawa@gmail.com>
2024-11-29 15:45:48 +0100

    refactor: combine integration tests to one parametrized test

--- packages/ragbits-core/tests/integration/vector_stores/test_vector_store.py
+++ packages/ragbits-core/tests/integration/vector_stores/test_vector_store.py
@@ -1,12 +1,35 @@
 from unittest.mock import AsyncMock
 
+import pytest
+from chromadb import EphemeralClient
+from qdrant_client import AsyncQdrantClient
+
+from ragbits.core.vector_stores.base import VectorStore
+from ragbits.core.vector_stores.chroma import ChromaVectorStore
 from ragbits.core.vector_stores.in_memory import InMemoryVectorStore
+from ragbits.core.vector_stores.qdrant import QdrantVectorStore
 from ragbits.document_search import DocumentSearch
 from ragbits.document_search.documents.document import DocumentMeta
 from ragbits.document_search.documents.sources import LocalFileSource
 
 
-async def test_handling_document_ingestion_with_different_content_and_verifying_replacement() -> None:
+@pytest.mark.parametrize(
+    "vector_store",
+    [
+        InMemoryVectorStore(),
+        ChromaVectorStore(
+            client=EphemeralClient(),
+            index_name="test_index_name",
+        ),
+        QdrantVectorStore(
+            client=AsyncQdrantClient(":memory:"),
+            index_name="test_index_name",
+        ),
+    ],
+)
+async def test_handling_document_ingestion_with_different_content_and_verifying_replacement(
+    vector_store: VectorStore,
+) -> None:
     document_1_content = "This is a test sentence and it should be in the vector store"
     document_2_content = "This is another test sentence and it should be removed from the vector store"
     document_2_new_content = "This is one more test sentence and it should be added to the vector store"
@@ -16,7 +39,6 @@ async def test_handling_document_ingestion_with_different_content_and_verifying_
 
     embedder = AsyncMock()
     embedder.embed_text.return_value = [[0.0], [0.0]]
-    vector_store = InMemoryVectorStore()
     document_search = DocumentSearch(
         embedder=embedder,
         vector_store=vector_store,
