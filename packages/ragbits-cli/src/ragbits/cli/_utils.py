03c96347ad547e60726dccf01db59a05744bc132
Ludwik Trammer <ludwik.trammer@deepsense.ai>
2024-12-18 21:23:48 +0100

    Workaround mypy with a Protocol

--- packages/ragbits-cli/src/ragbits/cli/_utils.py
+++ packages/ragbits-cli/src/ragbits/cli/_utils.py
@@ -1,24 +1,34 @@
 from pathlib import Path
-from typing import TypeVar
+from typing import Protocol, TypeVar
 
 import typer
 from pydantic.alias_generators import to_snake
 from rich.console import Console
 
-from ragbits.core.config import core_config
+from ragbits.core.config import CoreConfig, core_config
 from ragbits.core.utils.config_handling import InvalidConfigError, NoDefaultConfigError, WithConstructionConfig
 
-WithConstructionConfigT = TypeVar("WithConstructionConfigT", bound=WithConstructionConfig)
+WithConstructionConfigT_co = TypeVar("WithConstructionConfigT_co", bound=WithConstructionConfig, covariant=True)
+
+
+# Using a Protocol instead of simply typing the `cls` argument to `get_instance_or_exit`
+# as `type[WithConstructionConfigT]` in order to workaround the issue of mypy not allowing abstract classes
+# to be used as types: https://github.com/python/mypy/issues/4717
+class WithConstructionConfigProtocol(Protocol[WithConstructionConfigT_co]):
+    @classmethod
+    def subclass_from_defaults(
+        cls, defaults: CoreConfig, factory_path_override: str | None = None, yaml_path_override: Path | None = None
+    ) -> WithConstructionConfigT_co: ...
 
 
 def get_instance_or_exit(
-    cls: type[WithConstructionConfigT],
+    cls: WithConstructionConfigProtocol[WithConstructionConfigT_co],
     type_name: str | None = None,
     yaml_path: Path | None = None,
     factory_path: str | None = None,
     yaml_path_argument_name: str = "--yaml-path",
     factory_path_argument_name: str = "--factory-path",
-) -> WithConstructionConfigT:
+) -> WithConstructionConfigT_co:
     """
     Returns an instance of the provided class, initialized using its `subclass_from_defaults` method.
     If the instance can't be created, prints an error message and exits the program.
@@ -31,6 +41,9 @@ def get_instance_or_exit(
         yaml_path_argument_name: The name of the argument to use in error messages for the YAML path.
         factory_path_argument_name: The name of the argument to use in error messages for the factory path.
     """
+    if not isinstance(cls, type):
+        raise TypeError(f"get_instance_or_exit expects the `cls` argument to be a class, got {cls}")
+
     type_name = type_name or to_snake(cls.__name__).replace("_", " ")
     try:
         return cls.subclass_from_defaults(
