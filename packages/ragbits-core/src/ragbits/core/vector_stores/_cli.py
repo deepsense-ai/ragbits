44eccc81b0079f9ddb63c5619383d7abed26e3d6
Ludwik Trammer <ludwik.trammer@deepsense.ai>
2024-12-18 13:50:53 +0100

    feat(cli): add option to choose which columns to display

--- packages/ragbits-core/src/ragbits/core/vector_stores/_cli.py
+++ packages/ragbits-core/src/ragbits/core/vector_stores/_cli.py
@@ -1,6 +1,7 @@
 import asyncio
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Annotated
 
 import typer
 from pydantic import BaseModel
@@ -23,17 +24,28 @@ class CLIState:
 
 state: CLIState = CLIState()
 
+# Default columns for commands that list entries
+_default_columns = "id,key,metadata"
+
 
 @vector_stores_app.callback()
 def common_args(
-    factory_path: str | None = None,
-    yaml_path: str | None = None,
+    factory_path: Annotated[
+        str | None,
+        typer.Option(
+            help="Python path to a function that creates a vector store, ina format 'module.submodule:function'"
+        ),
+    ] = None,
+    yaml_path: Annotated[
+        Path | None,
+        typer.Option(help="Path to a YAML configuration file for the vector store", exists=True, resolve_path=True),
+    ] = None,
 ) -> None:
     try:
         state.vector_store = VectorStore.subclass_from_defaults(
             core_config,
             factory_path_override=factory_path,
-            yaml_path_override=Path.cwd() / yaml_path if yaml_path else None,
+            yaml_path_override=yaml_path,
         )
     except InvalidConfigError as e:
         Console(stderr=True).print(e)
@@ -41,7 +53,13 @@ def common_args(
 
 
 @vector_stores_app.command(name="list")
-def list_entries(limit: int = 10, offset: int = 0) -> None:
+def list_entries(
+    limit: Annotated[int, typer.Option(help="Maximum number of entries to list")] = 10,
+    offset: Annotated[int, typer.Option(help="How many entries to skip")] = 0,
+    columns: Annotated[
+        str, typer.Option(help="Comma-separated list of columns to display, aviailable: id, key, vector, metadata")
+    ] = _default_columns,
+) -> None:
     """
     List all objects in the chosen vector store.
     """
@@ -51,7 +69,7 @@ def list_entries(limit: int = 10, offset: int = 0) -> None:
             raise ValueError("Vector store not initialized")
 
         entries = await state.vector_store.list(limit=limit, offset=offset)
-        print_output(entries)
+        print_output(entries, columns=columns)
 
     asyncio.run(run())
 
@@ -61,7 +79,9 @@ class RemovedItem(BaseModel):
 
 
 @vector_stores_app.command()
-def remove(ids: list[str]) -> None:
+def remove(
+    ids: Annotated[list[str], typer.Argument(help="IDs of the entries to remove from the vector store")],
+) -> None:
     """
     Remove objects from the chosen vector store.
     """
@@ -81,11 +101,22 @@ def remove(ids: list[str]) -> None:
 
 @vector_stores_app.command()
 def query(
-    text: str,
-    k: int = 5,
-    max_distance: float | None = None,
-    embedder_factory_path: str | None = None,
-    embedder_yaml_path: str | None = None,
+    text: Annotated[str, typer.Argument(help="Text to query the vector store with")],
+    k: Annotated[int, typer.Option(help="Number of entries to retrieve")] = 5,
+    max_distance: Annotated[float | None, typer.Option(help="Maximum distance to the query vector")] = None,
+    embedder_factory_path: Annotated[
+        str | None,
+        typer.Option(
+            help="Python path to a function that creates an embedder, in a format 'module.submodule:function'"
+        ),
+    ] = None,
+    embedder_yaml_path: Annotated[
+        Path | None,
+        typer.Option(help="Path to a YAML configuration file for the embedder", exists=True, resolve_path=True),
+    ] = None,
+    columns: Annotated[
+        str, typer.Option(help="Comma-separated list of columns to display, aviailable: id, key, vector, metadata")
+    ] = _default_columns,
 ) -> None:
     """
     Query the chosen vector store.
@@ -99,7 +130,7 @@ def query(
             embedder = Embeddings.subclass_from_defaults(
                 core_config,
                 factory_path_override=embedder_factory_path,
-                yaml_path_override=Path.cwd() / embedder_yaml_path if embedder_yaml_path else None,
+                yaml_path_override=embedder_yaml_path,
             )
         except InvalidConfigError as e:
             Console(stderr=True).print(e)
@@ -112,6 +143,6 @@ def query(
             vector=search_vector[0],
             options=options,
         )
-        print_output(entries)
+        print_output(entries, columns=columns)
 
     asyncio.run(run())
