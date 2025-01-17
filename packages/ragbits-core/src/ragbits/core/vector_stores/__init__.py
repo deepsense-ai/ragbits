8dfa69685687d1386cdbd041c658ab3ada3be821
Michał Pstrąg <michal.pstrag@icloud.com>
2025-01-17 10:25:43 +0100

    revert change

--- packages/ragbits-core/src/ragbits/core/vector_stores/__init__.py
+++ packages/ragbits-core/src/ragbits/core/vector_stores/__init__.py
@@ -1,14 +1,4 @@
 from ragbits.core.vector_stores.base import VectorStore, VectorStoreEntry, VectorStoreOptions, WhereQuery
-from ragbits.core.vector_stores.chroma import ChromaVectorStore
 from ragbits.core.vector_stores.in_memory import InMemoryVectorStore
-from ragbits.core.vector_stores.qdrant import QdrantVectorStore
 
-__all__ = [
-    "ChromaVectorStore",
-    "InMemoryVectorStore",
-    "QdrantVectorStore",
-    "VectorStore",
-    "VectorStoreEntry",
-    "VectorStoreOptions",
-    "WhereQuery",
-]
+__all__ = ["InMemoryVectorStore", "VectorStore", "VectorStoreEntry", "VectorStoreOptions", "WhereQuery"]
