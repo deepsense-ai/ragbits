d1dbf634ab29d2039adfd8cf051495ca5e565786
kdziedzic68 <krzysztof.dziedzic@deepsense.ai>
2025-01-31 10:54:16 +0100

    fix ruff linter

--- packages/ragbits-core/src/ragbits/core/embeddings/sparse.py
+++ packages/ragbits-core/src/ragbits/core/embeddings/sparse.py
@@ -1,6 +1,10 @@
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 from collections import Counter
 from dataclasses import dataclass
+
+import tiktoken
+
+from ragbits.core.utils.config_handling import WithConstructionConfig
 
 
 @dataclass
@@ -21,7 +25,7 @@ class SparseVector:
         return f"SparseVector(non_zero_dims={self.non_zero_dims}, non_zero_vals={self.non_zero_vals}, dim={self.dim})"
 
 
-class SparseEmbedding:
+class SparseEmbedding(WithConstructionConfig, ABC):
     """Sparse embedding interface"""
 
     @abstractmethod
@@ -32,40 +36,34 @@ class SparseEmbedding:
 class BagofTokens(SparseEmbedding):
     """BagofTokens implementations of sparse Embeddings interface"""
 
-    def __init__(self, vocabulary: dict[str, int]):
+    def __init__(self, encoding_name: str):
         """
-        Initializes the BagOfTokens embedding. If no vocabulary is provided, it will be built automatically.
-        :param vocabulary: A mapping from token to its index in the embedding (optional).
+        Initializes the BagOfTokens embedding
+        Args:
+            encoding_name: Name of encoding to be used. Needs to be supported by tiktoken library.
         """
-        self.vocabulary = vocabulary or {}
-        self.dim = len(self.vocabulary)
+        self.encoder = tiktoken.get_encoding(encoding_name=encoding_name)
+        self.dim = self.encoder.n_vocab
 
-    def _build_vocabulary(self, texts: list[str]) -> None:
-        all_tokens = [token for text in texts for token in text.split()]  # also tokenizer?
-        unique_tokens = list(set(all_tokens))
-        self.vocabulary = {token: idx for idx, token in enumerate(unique_tokens)}
-        self.dim = len(self.vocabulary)
-
-    def embed(self, texts: list[str]) -> list[SparseVector]:
+    def embed_text(self, texts: list[str]) -> list[SparseVector]:
         """
         Transforms a list of texts into sparse vectors using bag-of-tokens representation.
-        If the vocabulary is not defined, it will be built from the texts.
-        :param texts: list of input texts.
-        :return: list of SparseVector instances.
-        """
-        if not self.vocabulary:
-            self._build_vocabulary(texts)
 
+        Args:
+            texts: list of input texts.
+
+        Returns:
+            list of SparseVector instances.
+        """
         vectors = []
         for text in texts:
-            tokens = text.split()  # maybe here tokenizer usage?
+            tokens = self.encoder.encode(text)
             token_counts = Counter(tokens)
-
             non_zero_dims = []
             non_zero_vals = []
 
             for token, count in token_counts.items():
-                non_zero_dims.append(self.vocabulary[token])
+                non_zero_dims.append(token)
                 non_zero_vals.append(count)
 
             vectors.append(SparseVector(non_zero_dims, non_zero_vals, self.dim))
