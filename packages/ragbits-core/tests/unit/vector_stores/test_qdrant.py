ac2492bb82d42792fad923fd9925bacb07217542
kzamlynska <katarzyna.zamlynska@deepsense.ai>
2025-02-12 21:21:45 +0100

    unit test added

--- packages/ragbits-core/tests/unit/vector_stores/test_qdrant.py
+++ packages/ragbits-core/tests/unit/vector_stores/test_qdrant.py
@@ -169,11 +169,11 @@ async def test_list(mock_qdrant_store: QdrantVectorStore) -> None:
 
 def test_create_qdrant_filter() -> None:
     where = {"a": "A", "b": 3, "c": True}
-    qdrant_filter = QdrantVectorStore._create_qdrant_filter(where) # type: ignore
+    qdrant_filter = QdrantVectorStore._create_qdrant_filter(where)  # type: ignore
     assert isinstance(qdrant_filter, models.Filter)
     expected_conditions = [
         models.FieldCondition(key="a", match=models.MatchValue(value="A")),
         models.FieldCondition(key="b", match=models.MatchValue(value=3)),
-        models.FieldCondition(key="c", match=models.MatchValue(value=True))
+        models.FieldCondition(key="c", match=models.MatchValue(value=True)),
     ]
     assert qdrant_filter.must == expected_conditions
