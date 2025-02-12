dd16ecfb0d98c38465a93695f74b5ef82ce0a208
kzamlynska <katarzyna.zamlynska@deepsense.ai>
2025-02-12 17:35:43 +0100

    corrct init

--- packages/ragbits-document-search/src/ragbits/document_search/documents/sources/__init__.py
+++ packages/ragbits-document-search/src/ragbits/document_search/documents/sources/__init__.py
@@ -1,5 +1,6 @@
 from ragbits.document_search.documents.sources.gcs_source import GCSSource
 from ragbits.document_search.documents.sources.hugging_face_source import HuggingFaceSource
 from ragbits.document_search.documents.sources.local_file_source import LocalFileSource
+from ragbits.document_search.documents.sources.sources import Source
 
-__all__ = ["GCSSource", "HuggingFaceSource", "LocalFileSource"]
+__all__ = ["GCSSource", "HuggingFaceSource", "LocalFileSource", "Source"]
