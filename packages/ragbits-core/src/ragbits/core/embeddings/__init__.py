11476d1ff1d0bfa9f58bd7489c87c65cf1607f38
nczerep <natalia.czerep@deepsense.ai>
2025-02-23 18:56:11 +0100

    feat: add sparse vectors representation
    Co-authored-by: kdziedzic68 <krzysztof.dziedzic@deepsense.ai>
    Co-authored-by: Ludwik Trammer <ludwik.trammer@deepsense.ai>
    Co-authored-by: Mateusz Hordyński <26008518+mhordynski@users.noreply.github.com>

--- packages/ragbits-core/src/ragbits/core/embeddings/__init__.py
+++ packages/ragbits-core/src/ragbits/core/embeddings/__init__.py
@@ -1,5 +1,16 @@
 from .base import Embeddings, EmbeddingsOptionsT, EmbeddingType
 from .litellm import LiteLLMEmbeddings
 from .noop import NoopEmbeddings
+from .sparse import BagOfTokens, SparseEmbeddings, SparseEmbeddingsOptionsT
 
-__all__ = ["EmbeddingType", "Embeddings", "EmbeddingsOptionsT", "LiteLLMEmbeddings", "NoopEmbeddings"]
+__all__ = [
+    "BagOfTokens",
+    "BagOfTokens",
+    "EmbeddingType",
+    "Embeddings",
+    "EmbeddingsOptionsT",
+    "LiteLLMEmbeddings",
+    "NoopEmbeddings",
+    "SparseEmbeddings",
+    "SparseEmbeddingsOptionsT",
+]
--- packages/ragbits-core/tests/unit/embeddings/test_from_config.py
+++ packages/ragbits-core/tests/unit/embeddings/test_from_config.py
@@ -1,10 +1,11 @@
 from ragbits.core.embeddings import Embeddings, NoopEmbeddings
 from ragbits.core.embeddings.litellm import LiteLLMEmbeddings, LiteLLMEmbeddingsOptions
+from ragbits.core.embeddings.sparse import BagOfTokens, BagOfTokensOptions, SparseEmbeddings
 from ragbits.core.types import NOT_GIVEN
 from ragbits.core.utils.config_handling import ObjectContructionConfig
 
 
-def test_subclass_from_config():
+def test_subclass_from_config_litellm():
     config = ObjectContructionConfig.model_validate(
         {
             "type": "ragbits.core.embeddings.litellm:LiteLLMEmbeddings",
@@ -30,7 +31,30 @@ def test_subclass_from_config():
     )  # type: ignore
 
 
-def test_subclass_from_config_default_path():
+def test_subclass_from_config_default_path_litellm():
     config = ObjectContructionConfig.model_validate({"type": "NoopEmbeddings"})
     embedding: Embeddings = Embeddings.subclass_from_config(config)
     assert isinstance(embedding, NoopEmbeddings)
+
+
+def test_subclass_from_config_bag_of_tokens():
+    config = ObjectContructionConfig.model_validate(
+        {
+            "type": "ragbits.core.embeddings.sparse:BagOfTokens",
+            "config": {
+                "default_options": {
+                    "option1": "value1",
+                    "option2": "value2",
+                },
+            },
+        }
+    )
+    embedding: SparseEmbeddings = SparseEmbeddings.subclass_from_config(config)
+    assert isinstance(embedding, BagOfTokens)
+    assert embedding.default_options == BagOfTokensOptions(
+        model_name="gpt-4o",
+        encoding_name=NOT_GIVEN,
+        min_token_count=NOT_GIVEN,
+        option1="value1",
+        option2="value2",
+    )  # type: ignore
