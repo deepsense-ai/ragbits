c88961c91692a82d03418b6a9274ba821a601c53
kdziedzic68 <krzysztof.dziedzic@deepsense.ai>
2025-03-05 13:57:47 +0100

    add testing json output for cli

--- packages/ragbits-evaluate/tests/cli/test_run_evaluation.py
+++ packages/ragbits-evaluate/tests/cli/test_run_evaluation.py
@@ -1,15 +1,25 @@
 import asyncio
+import json
 
+import pytest
 from typer.testing import CliRunner
 
 from ragbits.core.embeddings.noop import NoopEmbedder
 from ragbits.core.vector_stores.in_memory import InMemoryVectorStore
 from ragbits.document_search import DocumentSearch
 from ragbits.document_search.documents.document import DocumentMeta
-from ragbits.evaluate.cli import eval_app
+from ragbits.cli import app, autoregister
+from ragbits.evaluate.cli import eval_app, EvaluationResult
+
 
 factory_path = f"{__name__}:create_document_search_instance_with_documents"
 target_cls = "ragbits.document_search:DocumentSearch"
+
+
+@pytest.fixture()
+def dataloader_args() -> str:
+    """Arguments for dataloader"""
+    return "deepsense-ai/synthetic-rag-dataset_v1.0,train"
 
 
 async def _add_example_documents(document_search: DocumentSearch) -> None:
@@ -27,7 +37,7 @@ def create_document_search_instance_with_documents():
     return document_search
 
 
-def test_run_evaluation() -> None:
+def test_run_evaluation(dataloader_args: str) -> None:
     runner = CliRunner(mix_stderr=False)
     result = runner.invoke(
         eval_app,
@@ -37,8 +47,34 @@ def test_run_evaluation() -> None:
             "--target-cls",
             target_cls,
             "--dataloader-args",
-            "deepsense-ai/synthetic-rag-dataset_v1.0,train",
+            dataloader_args,
             "run-evaluation",
         ],
     )
     assert result.exit_code == 0
+
+
+def test_run_evaluation_json_output(dataloader_args: str) -> None:
+    autoregister()
+    runner = CliRunner(mix_stderr=False)
+    result = runner.invoke(
+        app,
+        [
+            "--output",
+            "json",
+            "evaluate",
+            "--target-factory-path",
+            factory_path,
+            "--target-cls",
+            target_cls,
+            "--dataloader-args",
+            dataloader_args,
+            "run-evaluation",
+        ],
+    )
+    assert result.exit_code == 0
+    parsed_result = json.loads(result.stdout)
+    assert isinstance(parsed_result, list)
+    assert len(parsed_result) == 1
+    assert EvaluationResult.model_validate(parsed_result[0])
+
