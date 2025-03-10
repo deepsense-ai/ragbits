4d75955ae3c11b696bd275feb547d38f6f474911
Michał Pstrąg <michal.pstrag@icloud.com>
2025-03-10 16:56:39 +0100

    small fix

--- packages/ragbits-core/src/ragbits/core/llms/litellm.py
+++ packages/ragbits-core/src/ragbits/core/llms/litellm.py
@@ -119,9 +119,8 @@ class LiteLLM(LLM[LiteLLMOptions]):
             LLMNotSupportingImagesError: If the model does not support images.
         """
         if prompt.list_images() and not litellm.supports_vision(self.model_name):
-            raise LLMNotSupportingImagesError(
-                f"There are images in the prompt, but given LLM ({self.model_name}) " f"doesn't support them."
-            )
+            raise LLMNotSupportingImagesError()
+
         conversation = prompt.chat
         response_format = self._get_response_format(output_schema=output_schema, json_mode=json_mode)
 
@@ -180,9 +179,8 @@ class LiteLLM(LLM[LiteLLMOptions]):
             LLMNotSupportingImagesError: If the model does not support images.
         """
         if prompt.list_images() and not litellm.supports_vision(self.model_name):
-            raise LLMNotSupportingImagesError(
-                f"There are images in the prompt, but given LLM ({self.model_name}) " f"doesn't support them."
-            )
+            raise LLMNotSupportingImagesError()
+
         conversation = prompt.chat
 
         response_format = self._get_response_format(output_schema=output_schema, json_mode=json_mode)
