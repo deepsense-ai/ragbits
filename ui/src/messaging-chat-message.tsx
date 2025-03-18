0088f0ddee6fb8e96b10b2a929e3b639665a78da
dazy-ds <135028324+dazy-ds@users.noreply.github.com>
2025-03-18 10:39:55 +0100

    feat: poc of plugin manager for UI (#418)
    Co-authored-by: jakubduda-dsai <jakub.duda@deepsense.ai>

---
+++ ui/src/messaging-chat-message.tsx
@@ -0,0 +1,54 @@
+import type { MessagingChatMessageProps } from "./data";
+
+import React from "react";
+import { cn } from "@heroui/react";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+const MessagingChatMessage = React.forwardRef<
+  HTMLDivElement,
+  MessagingChatMessageProps
+>(({ name, time, message, isRTL, className, classNames, ...props }, ref) => {
+  const messageRef = React.useRef<HTMLDivElement>(null);
+
+  const Message = () => (
+    <div className="flex max-w-[70%] flex-col gap-4">
+      <div
+        className={cn(
+          "relative w-full rounded-medium bg-content2 px-4 py-3 text-default-600",
+          classNames?.base,
+        )}
+      >
+        <div className="flex">
+          <div className="w-full text-small font-semibold text-default-foreground">
+            {name}
+          </div>
+          <div className="flex-end text-small text-default-400">{time}</div>
+        </div>
+        <div ref={messageRef} className="mt-2 text-small text-default-900">
+          {isRTL ? (
+            <div className="whitespace-pre-line">{message}</div>
+          ) : (
+            <Markdown className="prose max-w-full" remarkPlugins={[remarkGfm]}>
+              {message}
+            </Markdown>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <div
+      {...props}
+      ref={ref}
+      className={cn("flex gap-3", { "flex-row-reverse": isRTL }, className)}
+    >
+      <Message />
+    </div>
+  );
+});
+
+MessagingChatMessage.displayName = "MessagingChatMessage";
+
+export default MessagingChatMessage;
