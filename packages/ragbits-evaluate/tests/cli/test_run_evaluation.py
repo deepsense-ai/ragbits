9c7af53717992b83af85349665de23a11e6bc0a0
kdziedzic68 <krzysztof.dziedzic@deepsense.ai>
2025-03-18 11:55:25 +0100

    remove problematic test

--- packages/ragbits-evaluate/tests/cli/test_run_evaluation.py
+++ packages/ragbits-evaluate/tests/cli/test_run_evaluation.py
@@ -1,15 +1,13 @@
 import asyncio
-import json
 
 import pytest
 from typer.testing import CliRunner
 
-from ragbits.cli import app, autoregister
 from ragbits.core.embeddings.noop import NoopEmbedder
 from ragbits.core.vector_stores.in_memory import InMemoryVectorStore
 from ragbits.document_search import DocumentSearch
 from ragbits.document_search.documents.document import DocumentMeta
-from ragbits.evaluate.cli import EvaluationResult, eval_app
+from ragbits.evaluate.cli import eval_app
 
 factory_path = f"{__name__}:create_document_search_instance_with_documents"
 target_cls = "ragbits.document_search:DocumentSearch"
@@ -51,28 +49,3 @@ def test_run_evaluation(dataloader_args: str) -> None:
         ],
     )
     assert result.exit_code == 0
-
-
-def test_run_evaluation_json_output(dataloader_args: str) -> None:
-    autoregister()
-    runner = CliRunner(mix_stderr=False)
-    result = runner.invoke(
-        app,
-        [
-            "--output",
-            "json",
-            "evaluate",
-            "--target-factory-path",
-            factory_path,
-            "--target-cls",
-            target_cls,
-            "--dataloader-args",
-            dataloader_args,
-            "run-evaluation",
-        ],
-    )
-    assert result.exit_code == 0
-    parsed_result = json.loads(result.stdout)
-    assert isinstance(parsed_result, list)
-    assert len(parsed_result) == 1
-    assert EvaluationResult.model_validate(parsed_result[0])
