838b5338d8f1847a549765bd3f499fe6afe507a1
Maciej Kurzawa <68014263+mackurzawa@users.noreply.github.com>
2025-03-19 08:18:20 +0100

    fix: Rename HttpSource to WebSource (#422)

--- packages/ragbits-document-search/src/ragbits/document_search/documents/sources/web.py
+++ packages/ragbits-document-search/src/ragbits/document_search/documents/sources/web.py
@@ -9,41 +9,42 @@ with suppress(ImportError):
     import aiohttp
 
 from ragbits.core.utils.decorators import requires_dependencies
-from ragbits.document_search.documents.exceptions import SourceNotFoundError
+from ragbits.document_search.documents.exceptions import SourceNotFoundError, WebDownloadError
 from ragbits.document_search.documents.sources import Source
 from ragbits.document_search.documents.sources.base import get_local_storage_dir
 
 
-class HttpSource(Source):
+class WebSource(Source):
     """
-    An object representing an HTTP dataset source.
+    An object representing a Web dataset source.
     """
 
-    url: str
+    uri: str
     protocol: ClassVar[str] = "https"
 
     @property
     def id(self) -> str:
         """
-        Get the source ID, which is the full url to the file.
+        Get the source ID, which is an unique identifier of the object.
         """
-        return f"https:{self.url}"
+        return f"web:{self.uri}"
 
     @requires_dependencies(["aiohttp"])
     async def fetch(self) -> Path:
         """
-        Download a file available in the given url.
+        Download a file available in the given uri.
 
         Returns:
             Path: The local path to the downloaded file.
 
         Raises:
-            SourceNotFoundError: If the URL is invalid.
+            WebDownloadError: If the download failed.
+            SourceNotFoundError: If the URI is invalid.
         """
-        parsed_url = urlparse(self.url)
-        url_path, file_name = ("/" + parsed_url.netloc + parsed_url.path).rsplit("/", 1)
+        parsed_uri = urlparse(self.uri)
+        url_path, file_name = ("/" + parsed_uri.netloc + parsed_uri.path).rsplit("/", 1)
         normalized_url_path = re.sub(r"\W", "_", url_path) + file_name
-        domain_name = parsed_url.netloc
+        domain_name = parsed_uri.netloc
 
         local_dir = get_local_storage_dir()
         container_local_dir = local_dir / domain_name
@@ -51,42 +52,42 @@ class HttpSource(Source):
         path = container_local_dir / normalized_url_path
 
         try:
-            async with aiohttp.ClientSession() as session, session.get(self.url) as response:
+            async with aiohttp.ClientSession() as session, session.get(self.uri) as response:
                 if response.ok:
                     with open(path, "wb") as f:
                         async for chunk in response.content.iter_chunked(1024):
                             f.write(chunk)
                 else:
-                    raise SourceNotFoundError(self.id)
+                    raise WebDownloadError(uri=self.uri, code=response.status)
         except (aiohttp.ClientError, IsADirectoryError) as e:
             raise SourceNotFoundError(self.id) from e
 
         return path
 
     @classmethod
-    async def list_sources(cls, url: str) -> Sequence["HttpSource"]:
+    async def list_sources(cls, uri: str) -> Sequence["WebSource"]:
         """
-        List the file under the given URL.
+        List the file under the given URI.
 
         Arguments:
-            url: The URL to the file.
+            uri: The URI to the file.
 
         Returns:
-            Sequence: The Sequence with HTTP source.
+            Sequence: The Sequence with Web source.
         """
-        return [cls(url=url)]
+        return [cls(uri=uri)]
 
     @classmethod
-    async def from_uri(cls, url: str) -> Sequence["HttpSource"]:
+    async def from_uri(cls, uri: str) -> Sequence["WebSource"]:
         """
-        Create HttpSource instances from a URI path.
-        The supported url format is:
+        Create WebSource instances from a URI path.
+        The supported uri format is:
         <protocol>://<domain>/<path>/<filename>.<file_extension>
 
         Args:
-            url: The URI path.
+            uri: The URI path.
 
         Returns:
-            A sequence containing a HttpSource instance.
+            A sequence containing a WebSource instance.
         """
-        return [cls(url=url)]
+        return [cls(uri=uri)]
