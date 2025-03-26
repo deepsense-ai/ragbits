175fb6c45177c3c713fb43b936815b028ef4e53b
jakubduda-dsai <jakub.duda@deepsense.ai>
2025-03-26 08:36:27 +0100

    UI build

---
+++ packages/ragbits-api/src/ragbits/api/ui-build/assets/ExamplePluginComponent-CFBfqgIu.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CydtRWTo.js";const i=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"Example Plugin"}),e.jsx("p",{children:"This is an example plugin."})]});export{i as default};
