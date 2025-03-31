ba6a7e1f1f3a07cc6accb7594cb3fa3d64bf25ce
Mateusz Hordyński <mateusz.hordynski@deepsense.ai>
2025-03-31 13:43:41 +0200

    fix tests

--- packages/ragbits-core/tests/unit/embeddings/test_from_config.py
+++ packages/ragbits-core/tests/unit/embeddings/test_from_config.py
@@ -12,7 +12,7 @@ def test_subclass_from_config_litellm():
         {
             "type": "ragbits.core.embeddings.litellm:LiteLLMEmbedder",
             "config": {
-                "model": "some_model",
+                "model_name": "some_model",
                 "default_options": {
                     "option1": "value1",
                     "option2": "value2",
@@ -66,7 +66,7 @@ def test_from_config_with_router():
     config = ObjectConstructionConfig(
         type="ragbits.core.embeddings.litellm:LiteLLMEmbedder",
         config={
-            "model": "text-embedding-3-small",
+            "model_name": "text-embedding-3-small",
             "api_key": "test_api_key",
             "router": [
                 {
