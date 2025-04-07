4403226c5bb749ffaee1a8ee1a9d57b292f27072
jakubduda-dsai <148433294+jakubduda-dsai@users.noreply.github.com>
2025-04-07 15:45:04 +0200

    fix: Markdown styling (#500)
    Co-authored-by: Dawid Żywczak <dawid.zywczak@deepsense.ai>

--- ui/src/core/components/ChatMessage.tsx
+++ ui/src/core/components/ChatMessage.tsx
@@ -6,6 +6,8 @@ import type { ChatMessage } from "../../types/chat";
 import { MessageRole } from "../../types/api";
 import { Icon } from "@iconify/react";
 import DelayedTooltip from "./DelayedTooltip";
+import { useThemeContext } from "../../contexts/ThemeContext/useThemeContext.ts";
+import { Theme } from "../../contexts/ThemeContext/ThemeContext.ts";
 
 export type ChatMessageProps = {
   classNames?: string[];
@@ -19,6 +21,7 @@ const ChatMessage = forwardRef<HTMLDivElement, ChatMessageProps>(
     const [didAnimate, setDidAnimate] = useState(false);
     const copyIconTimerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
     const [copyIcon, setCopyIcon] = useState("heroicons:clipboard");
+    const { theme } = useThemeContext();
 
     const onCopyClick = () => {
       navigator.clipboard.writeText(content);
@@ -45,47 +48,56 @@ const ChatMessage = forwardRef<HTMLDivElement, ChatMessageProps>(
             !didAnimate && "motion-safe:animate-pop-in",
             "flex flex-col gap-4",
             rightAlign && "max-w-[75%]",
+            !rightAlign && "w-full",
           )}
           onAnimationEnd={() => setDidAnimate(true)}
         >
           <div
             className={cn(
-              "relative w-full rounded-medium px-4 py-3 text-default",
+              "relative rounded-medium px-4 py-3 text-default",
               rightAlign && "bg-default-100",
             )}
           >
-            <div className={cn("text-small text-default-900")}>
-              {rightAlign ? (
-                <div className="whitespace-pre-line">{content}</div>
-              ) : (
-                <>
-                  <Markdown
-                    className="max-w-full text-default-900"
-                    remarkPlugins={[remarkGfm]}
-                  >
-                    {content}
-                  </Markdown>
-                  <div className="mt-2 flex items-center gap-2">
-                    <DelayedTooltip content="Copy" placement="bottom">
-                      <Button
-                        isIconOnly
-                        variant="ghost"
-                        className="p-0"
-                        aria-label="Copy message"
-                        onPress={onCopyClick}
-                      >
-                        <Icon icon={copyIcon} />
-                      </Button>
-                    </DelayedTooltip>
-                    {!!onOpenFeedbackForm && (
-                      <Button variant="ghost" onPress={onOpenFeedbackForm}>
-                        Open Feedback Form
-                      </Button>
-                    )}
-                  </div>
-                </>
-              )}
-            </div>
+            {rightAlign ? (
+              <div
+                className={cn(
+                  "prose whitespace-pre-line",
+                  theme === Theme.DARK && "dark:prose-invert",
+                )}
+              >
+                {content}
+              </div>
+            ) : (
+              <>
+                <Markdown
+                  className={cn(
+                    "markdown-container prose max-w-full",
+                    theme === Theme.DARK && "dark:prose-invert",
+                  )}
+                  remarkPlugins={[remarkGfm]}
+                >
+                  {content}
+                </Markdown>
+                <div className="mt-2 flex items-center gap-2">
+                  <DelayedTooltip content="Copy" placement="bottom">
+                    <Button
+                      isIconOnly
+                      variant="ghost"
+                      className="p-0"
+                      aria-label="Copy message"
+                      onPress={onCopyClick}
+                    >
+                      <Icon icon={copyIcon} />
+                    </Button>
+                  </DelayedTooltip>
+                  {!!onOpenFeedbackForm && (
+                    <Button variant="ghost" onPress={onOpenFeedbackForm}>
+                      Open Feedback Form
+                    </Button>
+                  )}
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
