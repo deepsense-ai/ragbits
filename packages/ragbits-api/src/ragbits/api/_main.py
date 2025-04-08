e7ee82e075a838280d335f76335920234c26a484
Dawid Żywczak <dawid.zywczak@deepsense.ai>
2025-04-08 10:29:59 +0200

    fix: use relative paths in requests

--- packages/ragbits-api/src/ragbits/api/_main.py
+++ packages/ragbits-api/src/ragbits/api/_main.py
@@ -65,7 +65,7 @@ class RagbitsAPI:
                 "http://localhost:8000",
                 "http://localhost:5173",
                 "http://localhost:8081",
-                "http://127.0.1:8000",
+                "http://127.0.0.1:8000",
                 "http://127.0.0.1:5173",
                 "http://127.0.0.1:8081",
             ],
