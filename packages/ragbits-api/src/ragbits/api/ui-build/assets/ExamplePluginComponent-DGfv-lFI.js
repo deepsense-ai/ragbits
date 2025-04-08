c9ee0907fdce2de326933c5cf6838d00e176ee4f
Dawid Żywczak <dawid.zywczak@deepsense.ai>
2025-04-08 11:36:49 +0200

    fix: build process, remove old files

--- packages/ragbits-api/src/ragbits/api/ui-build/assets/ExamplePluginComponent-DGfv-lFI.js
+++
@@ -1,1 +0,0 @@
-import{H as e}from"./index-DM8u9t7p.js";const i=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"Example Plugin"}),e.jsx("p",{children:"This is an example plugin."})]});export{i as default};
--- packages/ragbits-api/src/ragbits/api/ui-build/assets/ExamplePluginComponent-oYzYOudi.js
+++
@@ -1,1 +0,0 @@
-import{H as e}from"./index-B6WKhxma.js";const i=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"Example Plugin"}),e.jsx("p",{children:"This is an example plugin."})]});export{i as default};
