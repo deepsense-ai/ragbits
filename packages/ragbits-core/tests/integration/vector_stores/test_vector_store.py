f8c344e9429f0952699ceb1770411104ac67ae76
Ludwik Trammer <ludwik.trammer@deepsense.ai>
2025-04-25 14:13:00 +0200

    CI test

--- packages/ragbits-core/tests/integration/vector_stores/test_vector_store.py
+++ packages/ragbits-core/tests/integration/vector_stores/test_vector_store.py
@@ -1,4 +1,3 @@
-import os
 from collections.abc import AsyncGenerator
 from functools import partial
 from pathlib import Path
@@ -8,6 +7,7 @@ from uuid import UUID
 import asyncpg
 import pytest
 from chromadb import EphemeralClient
+from psycopg import Connection
 from qdrant_client import AsyncQdrantClient
 
 from ragbits.core.embeddings.dense import NoopEmbedder
@@ -39,15 +39,8 @@ IMAGES_PATH = Path(__file__).parent.parent.parent / "assets" / "img"
 
 
 @pytest.fixture(name="pgvector_test_db")
-async def pgvector_test_db_fixture(request: pytest.FixtureRequest) -> AsyncGenerator[asyncpg.Pool, None]:
-    if os.getenv("GITHUB_ACTIONS") == "true":
-        # in CI, connect to a separate service
-        pg_info = request.getfixturevalue("postgresql_noproc")
-    else:
-        # in local dev, use the local postgres process
-        pg_info = request.getfixturevalue("postgresql").info
-
-    dsn = f"postgresql://{pg_info.user}:{pg_info.password}@{pg_info.host}:{pg_info.port}/{pg_info.dbname}"
+async def pgvector_test_db_fixture(postgresql: Connection) -> AsyncGenerator[asyncpg.Pool, None]:
+    dsn = f"postgresql://{postgresql.info.user}:{postgresql.info.password}@{postgresql.info.host}:{postgresql.info.port}/{postgresql.info.dbname}"
     async with asyncpg.create_pool(dsn) as pool:
         yield pool
 
