be166b4bfddf8653a8186a93ae9b2695dad86bba
Michał Pstrąg <47692610+micpst@users.noreply.github.com>
2025-05-13 16:57:07 +0200

    add tests for batched

---
+++ packages/ragbits-core/tests/unit/utils/test_helpers.py
@@ -0,0 +1,24 @@
+import pytest
+from ragbits.core.utils.helpers import batched
+
+
+@pytest.mark.parametrize(
+    "input_data, batch_size, expected",
+    [
+        ([], 3, []),
+        ([1, 2, 3], 5, [[1, 2, 3]]),
+        ([1, 2, 3], 3, [[1, 2, 3]]),
+        ([1, 2, 3, 4, 5, 6], 2, [[1, 2], [3, 4], [5, 6]]),
+        ([1, 2, 3, 4, 5], 2, [[1, 2], [3, 4], [5]]),
+    ],
+    ids=[
+        "empty_iterable",
+        "batch_size_larger_than_data",
+        "batch_size_equal_to_data",
+        "batch_size_divides_data_evenly",
+        "batch_size_with_remainder",
+    ]
+)
+def test_batched(input_data: list[int], batch_size: int, expected: list[list[int]]) -> None:
+    result = list(batched(input_data, batch_size))
+    assert result == expected
