a1f5549ab7b9ccd64ca9915f33a5592875e828ed
jakubduda-dsai <jakub.duda@deepsense.ai>
2025-05-13 11:58:11 +0200

    Add Prettier format and format:check command, formatted project files

--- ui/src/types/api.ts
+++ ui/src/types/api.ts
@@ -18,7 +18,7 @@ export interface Reference {
 export enum ChatResponseType {
   TEXT = "text",
   REFERENCE = "reference",
-  MESSAGE_ID = "message_id"
+  MESSAGE_ID = "message_id",
 }
 
 export interface ChatRequest {
@@ -47,7 +47,10 @@ interface ReferenceChatResponse {
   content: Reference;
 }
 
-export type ChatResponse = TextChatResponse | ReferenceChatResponse | MessageIdChatResponse;
+export type ChatResponse =
+  | TextChatResponse
+  | ReferenceChatResponse
+  | MessageIdChatResponse;
 
 export enum FormFieldType {
   TEXT = "text",
