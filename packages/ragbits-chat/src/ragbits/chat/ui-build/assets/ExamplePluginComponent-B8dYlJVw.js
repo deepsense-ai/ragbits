dd6c3fe4269aca426b816e4424039a2c094d3ae6
Dawid Żywczak <dawid.zywczak@deepsense.ai>
2025-05-21 16:00:47 +0200

    fix: CI/CD pipeline

---
+++ packages/ragbits-chat/src/ragbits/chat/ui-build/assets/ExamplePluginComponent-B8dYlJVw.js
@@ -0,0 +1,1 @@
+import{H as e}from"./index-wDtY7v-v.js";const i=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"Example Plugin"}),e.jsx("p",{children:"This is an example plugin."})]});export{i as default};
