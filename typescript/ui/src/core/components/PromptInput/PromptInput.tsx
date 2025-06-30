789272be6036ce2b5b5c25c9c49d851468c398c9
Dawid Żywczak <dawid.zywczak@deepsense.ai>
2025-06-30 10:53:49 +0200

    feat(ui): add quick messages using arrows

--- typescript/ui/src/core/components/PromptInput/PromptInput.tsx
+++ typescript/ui/src/core/components/PromptInput/PromptInput.tsx
@@ -9,17 +9,25 @@ import {
   useRef,
   ReactNode,
   useState,
+  useEffect,
+  useMemo,
 } from "react";
 
 import PromptInputText from "./PromptInputText";
 import { TextAreaProps } from "@heroui/react";
 import HorizontalActions from "./HorizontalActions";
+import { useCaretLogicalLineDetection } from "../../utils/useTextAreaCaretDetection";
+import { ChatMessage } from "../../../types/history";
+import { MessageRole } from "@ragbits/api-client-react";
 
 interface PromptInputProps {
   submit: (text: string) => void;
   stopAnswering: () => void;
+  onArrowUp?: (isFirstLine: boolean) => void;
+  onArrowDown?: (isLastLine: boolean) => void;
   isLoading: boolean;
   followupMessages?: string[] | null;
+  history?: ChatMessage[];
   formProps?: FormProps;
   inputProps?: TextAreaProps;
   sendButtonProps?: ButtonProps;
@@ -37,9 +45,54 @@ const PromptInput = ({
   sendButtonProps,
   customSendIcon,
   customStopIcon,
+  history,
 }: PromptInputProps) => {
   const [message, setMessage] = useState("");
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const { isCaretInFirstLine, isCaretInLastLine } =
+    useCaretLogicalLineDetection();
+  const historyLength = useMemo(() => history?.length ?? 0, [history?.length]);
+  const quickMessageIndex = useRef(Math.max(historyLength - 2, 0));
+
+  const setQuickMessage = useCallback(() => {
+    if (!history) {
+      return;
+    }
+
+    const quickMessage = history[quickMessageIndex.current];
+    if (!quickMessage || quickMessage.role !== MessageRole.USER) {
+      return;
+    }
+
+    setMessage(quickMessage.content);
+  }, [history]);
+
+  const onArrowUp = useCallback(
+    (isFirst: boolean) => {
+      if (!isFirst) {
+        return;
+      }
+
+      quickMessageIndex.current = Math.max(quickMessageIndex.current - 2, 0);
+      setQuickMessage();
+    },
+    [setQuickMessage],
+  );
+
+  const onArrowDown = useCallback(
+    (isLast: boolean) => {
+      if (!isLast) {
+        return;
+      }
+
+      quickMessageIndex.current = Math.min(
+        Math.max(historyLength - 2, 0),
+        quickMessageIndex.current + 2,
+      );
+      setQuickMessage();
+    },
+    [historyLength, setQuickMessage],
+  );
 
   const handleSubmit = useCallback(() => {
     if (!message && !isLoading) return;
@@ -60,13 +113,29 @@ const PromptInput = ({
 
   const handleKeyDown = useCallback(
     (e: KeyboardEvent<HTMLInputElement>) => {
+      if (!textAreaRef.current) return;
+
+      if (e.key === "ArrowUp" && onArrowUp) {
+        onArrowUp(isCaretInFirstLine(textAreaRef.current));
+      }
+
+      if (e.key === "ArrowDown" && onArrowDown) {
+        onArrowDown(isCaretInLastLine(textAreaRef.current));
+      }
+
       if (e.key === "Enter" && !e.shiftKey) {
         e.preventDefault();
 
         handleSubmit();
       }
     },
-    [handleSubmit],
+    [
+      handleSubmit,
+      isCaretInFirstLine,
+      isCaretInLastLine,
+      onArrowDown,
+      onArrowUp,
+    ],
   );
 
   const handleStopAnswering = useCallback(() => {
@@ -74,12 +143,24 @@ const PromptInput = ({
     textAreaRef?.current?.focus();
   }, [stopAnswering]);
 
+  const sendFollowupMessage = useCallback(
+    (text: string) => {
+      setMessage("");
+      submit(text);
+    },
+    [submit],
+  );
+
+  useEffect(() => {
+    quickMessageIndex.current = Math.max(0, historyLength);
+  }, [historyLength]);
+
   return (
     <div className="rounded-medium">
       <HorizontalActions
         isVisible={!!followupMessages}
         actions={followupMessages ?? []}
-        sendMessage={submit}
+        sendMessage={sendFollowupMessage}
       />
 
       <Form
@@ -92,7 +173,7 @@ const PromptInput = ({
           ref={textAreaRef}
           aria-label="Message to the chat"
           classNames={{
-            input: "text-medium",
+            input: "text-medium text-default-foreground",
             inputWrapper:
               "!bg-transparent shadow-none group-data-[focus-visible=true]:ring-0 group-data-[focus-visible=true]:ring-offset-0 py-4",
           }}
