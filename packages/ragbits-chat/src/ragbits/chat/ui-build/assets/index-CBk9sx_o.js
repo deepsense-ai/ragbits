91b127c4b333068933b1a470f0f77fdaa21d2449
Dawid Żywczak <dawid.zywczak@deepsense.ai>
2025-07-07 09:53:34 +0200

    fix(ui): git diff step to check only ui folder in shared-ui.yml

--- packages/ragbits-chat/src/ragbits/chat/ui-build/assets/index-CBk9sx_o.js
+++
@@ -1,1 +0,0 @@
-import{aO as a,aP as e,aQ as t}from"./index-CR2DCuxP.js";const n={renderer:t,...e,...a};var o=n;export{o as default};
---
+++ packages/ragbits-chat/src/ragbits/chat/ui-build/assets/index-DDGupVHw.js
@@ -0,0 +1,1 @@
+import{aO as a,aP as e,aQ as t}from"./index-D6enJ4QQ.js";const n={renderer:t,...e,...a};var o=n;export{o as default};
