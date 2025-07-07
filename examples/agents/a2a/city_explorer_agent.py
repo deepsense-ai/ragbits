c8ea652d6426a6979685e404b004b0e3841984ad
alicja <alicja.kotyla@deepsense.ai>
2025-07-07 09:34:38 +0200

    fix MCP example

--- examples/agents/a2a/city_explorer_agent.py
+++ examples/agents/a2a/city_explorer_agent.py
@@ -3,8 +3,8 @@ from pydantic import BaseModel
 from ragbits.agents import Agent
 from ragbits.agents.a2a.server import create_agent_server
 from ragbits.core.llms import LiteLLM
+from ragbits.agents.mcp import MCPServerStdio
 from ragbits.core.prompt import Prompt
-
 
 class CityExplorerPromptInput(BaseModel):
     """Defines the structured input schema for the city explorer prompt."""
@@ -30,15 +30,13 @@ class CityExplorerPrompt(Prompt[CityExplorerPromptInput]):
     """
 
 
-class MCPServerStdio:
-    pass
-
 async def main() -> None:
     async with MCPServerStdio(
             params={
-                "command": "python",
-                "args": ["-m", "mcp-server-fetch"],
-            }
+                "command": "uvx",
+                "args": ["mcp-server-fetch"],
+            },
+            client_session_timeout_seconds=10
         ) as server:
         llm = LiteLLM(
             model_name="gpt-4o-2024-08-06",
@@ -46,7 +44,7 @@ async def main() -> None:
         )
         city_explorer_agent = Agent(llm=llm, prompt=CityExplorerPrompt, mcp_servers=[server])
 
-        city_explorer_agent_card = city_explorer_agent.get_agent_card(
+        city_explorer_agent_card = await city_explorer_agent.get_agent_card(
             name="City Explorer Agent",
             description="Provides information about a city.",
         )
