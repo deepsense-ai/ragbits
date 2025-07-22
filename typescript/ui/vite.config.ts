a00fdb1f2154b22824f8c13a2a0d9e6b7683aefc
jakubduda-dsai <jakub.duda@deepsense.ai>
2025-07-22 15:57:24 +0200

    ui build, changelog

--- typescript/ui/vite.config.ts
+++ typescript/ui/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: "../../packages/ragbits-chat/src/ragbits/chat/ui-build",
     emptyOutDir: true,
