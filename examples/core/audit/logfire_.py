efc67dba28ea3dadf97b6c48292a836a1ab315ea
Mateusz Hordyński <mateusz.hordynski@deepsense.ai>
2025-08-02 19:07:24 +0200

    fix indifinite examples

--- examples/core/audit/logfire_.py
+++ examples/core/audit/logfire_.py
@@ -140,7 +140,7 @@ async def main() -> None:
     """
 
     async def run() -> AsyncGenerator:
-        while True:
+        for _ in range(5):
             await process_request()
             yield
 
