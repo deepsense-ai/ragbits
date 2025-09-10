c98d66721aa804905895668ac7e927f710a170d1
dazy-ds <135028324+dazy-ds@users.noreply.github.com>
2025-09-10 12:48:51 +0200

    refactor(ui): event handling refactor (#805)

--- scripts/generate_typescript_from_json_schema.py
+++ scripts/generate_typescript_from_json_schema.py
@@ -203,18 +203,11 @@ def _generate_chat_response_union_type() -> str:
         ("MessageUsageChatResponse", "usage", "Record<string, MessageUsage>"),
     ]
 
-    exported_response_interfaces = [
+    internal_response_interfaces = [
         ("ChunkedChatResponse", "chunked_content", "ChunkedContent"),
     ]
 
-    for interface_name, response_type, content_type in response_interfaces:
-        lines.append(f"interface {interface_name} {{")
-        lines.append(f"    type: '{response_type}'")
-        lines.append(f"    content: {content_type}")
-        lines.append("}")
-        lines.append("")
-
-    for interface_name, response_type, content_type in exported_response_interfaces:
+    for interface_name, response_type, content_type in [*response_interfaces, *internal_response_interfaces]:
         lines.append(f"export interface {interface_name} {{")
         lines.append(f"    type: '{response_type}'")
         lines.append(f"    content: {content_type}")
