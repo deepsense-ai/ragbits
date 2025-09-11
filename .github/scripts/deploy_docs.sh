4ea69ed38b011f717187db0e0dbda0b5c5aabc0b
Mateusz Hordyński <26008518+mhordynski@users.noreply.github.com>
2025-09-11 15:17:54 +0200

    chore: setup nightly releases (#808)

--- .github/scripts/deploy_docs.sh
+++ .github/scripts/deploy_docs.sh
@@ -6,12 +6,10 @@ echo $GCP_KEY | base64 -d >> gcp_creds.json
 gcloud auth activate-service-account --key-file gcp_creds.json
 gcloud config set project ds-internal-db-ally
 
-# Build the documentation
-uv run mkdocs build
-
 # Upload built docs to a bucket
-gcloud storage cp -r site/* gs://ragbits-documentation
+gcloud storage rsync . gs://ragbits-documentation --recursive --delete-unmatched-destination-objects
 
 # Invalidate cached content in the CDN
 gcloud compute url-maps invalidate-cdn-cache ragbits-documentation-lb \
     --path "/*" --async
+
