8e9331759def58fc020a1a3b44a124cb8713181d
Mateusz Hordyński <mateusz.hordynski@deepsense.ai>
2025-09-12 22:00:37 +0200

    add pipelines

---
+++ .github/scripts/deploy_docs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+echo $GCP_KEY | base64 -d >> gcp_creds.json
+gcloud auth activate-service-account --key-file gcp_creds.json
+gcloud config set project ds-internal-db-ally
+
+# Upload built docs to a bucket
+gcloud storage rsync . gs://ragbits-documentation --recursive --exclude '^.github/.*' --delete-unmatched-destination-objects
+
+# Invalidate cached content in the CDN
+gcloud compute url-maps invalidate-cdn-cache ragbits-documentation-lb \
+    --path "/*" --async
+
