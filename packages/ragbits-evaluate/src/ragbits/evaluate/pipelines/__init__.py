c52cfa8c2f942ab7332cb56b9a8faeea48ac8a40
rk-izak <102994642+rk-izak@users.noreply.github.com>
2025-10-16 14:00:20 +0200

    feat: add example evaluation pipelines targeting agents (#831)
    Co-authored-by: jakubduda-dsai <jakub.duda@deepsense.ai>
    Co-authored-by: Mateusz Hordyński <26008518+mhordynski@users.noreply.github.com>
    Co-authored-by: Maciej Kurzawa <68014263+mackurzawa@users.noreply.github.com>
    Co-authored-by: mackurzawa <mac.kurzawa@gmail.com>

--- packages/ragbits-evaluate/src/ragbits/evaluate/pipelines/__init__.py
+++ packages/ragbits-evaluate/src/ragbits/evaluate/pipelines/__init__.py
@@ -2,8 +2,19 @@ from ragbits.core.utils.config_handling import WithConstructionConfig
 from ragbits.document_search import DocumentSearch
 from ragbits.evaluate.pipelines.base import EvaluationData, EvaluationPipeline, EvaluationResult
 from ragbits.evaluate.pipelines.document_search import DocumentSearchPipeline
+from ragbits.evaluate.pipelines.gaia import GaiaPipeline
+from ragbits.evaluate.pipelines.hotpot_qa import HotpotQAPipeline
+from ragbits.evaluate.pipelines.human_eval import HumanEvalPipeline
 
-__all__ = ["DocumentSearchPipeline", "EvaluationData", "EvaluationPipeline", "EvaluationResult"]
+__all__ = [
+    "DocumentSearchPipeline",
+    "EvaluationData",
+    "EvaluationPipeline",
+    "EvaluationResult",
+    "GaiaPipeline",
+    "HotpotQAPipeline",
+    "HumanEvalPipeline",
+]
 
 _target_to_evaluation_pipeline: dict[type[WithConstructionConfig], type[EvaluationPipeline]] = {
     DocumentSearch: DocumentSearchPipeline,
